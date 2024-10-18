16e4875f7d5df473374be63b1b790ee3a7e2ab08
Yehor Riaboshapka <72949874+Yyooogurt@users.noreply.github.com>
2024-10-18 19:28:44 +0200

    Updated

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,14 @@
+import img from "./error.gif";
+
+const ErrorMessage = () => {
+    return (
+        <img
+            alt="error"
+            src={img}
+            style={{ display: "block", width: "250px", height: "250px", objectFit: "contain", margin: "0 auto" }}
+        />
+        // <img src={process.env.PUBLIC_URL + '/error.gif'}/>
+    );
+};
+
+export default ErrorMessage;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
     _apiBase = "https://gateway.marvel.com:443/v1/public/";
-    _apiKey = "";
+    _apiKey = "21c28b5fae817d3f01eb4f993dd18422";
 
     getResource = async (url) => {
         const res = await fetch(url);
@@ -14,12 +14,22 @@ class MarvelService {
 
     getAllCharacters = async () => {
         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
-        return res;
+        return res.data.results.map(this._transformCharacter);
     };
 
     getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}/?apikey=${this._apiKey}`);
-        return res;
+        const res = await this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    };
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        };
     };
 }
 
